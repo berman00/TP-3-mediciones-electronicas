a352ae58528013f74d9bb55ed69ba36f8894b6fc
Valentin Berman <valentin.berman@dlr.de>
2025-02-12 12:17:10 -0300

    actualizacion manual de cuentas

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,33 @@
 
 #define T_MUESTREO 1000 // ms
 
+
+#include <CmdParser.hpp>
+#include <CmdBuffer.hpp>
+#include <CmdCallback.hpp>
+
+CmdParser cmdParser;
+CmdBuffer<32> myBuffer;
+CmdCallback<1> cmdCallback;
+
+void setCuentasAdc(CmdParser *parser);
 bool botonPresionado();
 float getTemperatura();
 
 uint32_t ult_conversion_ms;
+uint16_t cuentas_adc_manual;
 
 void setup() {
+
+    // CLI
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("Serial listo");
+
+    cmdCallback.addCmd(PSTR("CUENTAS"), &setCuentasAdc);
+    myBuffer.setEcho(true);
+
+
 
     // Para usar con bateria
     pinMode(15, OUTPUT);
@@ -37,8 +58,17 @@ void setup() {
 
 void loop() {
 
+    // CLI
+    cmdCallback.updateCmdProcessing(&cmdParser, &myBuffer, &Serial);
+
+    // Display
     Display.updateDisplay();
-  
+    
+    if(botonPresionado()){
+        Display.toggleModo();
+    }
+
+    // Muestreo
     if(millis() - ult_conversion_ms > T_MUESTREO) {
 
         ult_conversion_ms = millis();
@@ -46,9 +76,6 @@ void loop() {
         Display.setTemp(getTemperatura());
     }
 
-    if(botonPresionado()){
-        Display.toggleModo();
-    }
 
 }
 
@@ -84,7 +111,7 @@ bool botonPresionado(){
 
 float getTemperatura() {
 
-    int16_t cuentas = analogRead(PIN_ADC);
+    int16_t cuentas = cuentas_adc_manual;
 
     constexpr int16_t max_cuentas = 0xFFF; // ADC de 12 bits
     constexpr int16_t offset = 6;          // cuando la temp es 0 C la salida del AO de instrumental es 5mv y no puede llegar nunca a 0V
@@ -92,4 +119,15 @@ float getTemperatura() {
     constexpr float max_temp = 100.0;
 
     return ((cuentas - offset) * max_temp) / ((float)max_cuentas);
+}
+
+void setCuentasAdc(CmdParser *parser) {
+
+    if (parser->getParamCount() != 2) {
+        Serial.println("Especificar el valor de cuentas ADC");
+        return;
+    }
+
+    String param_string = String(parser->getCmdParam(1));
+    cuentas_adc_manual = param_string.toInt();
 }
